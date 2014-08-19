0385be8a357826fd240e555d2f7eceb586ce9ac9
CP10 <cristian930714@hotmail.com>
2014-08-19 09:37:36 -0500

    Version2
    habilitado el landscape, idiomas español e ingles, agregado tema para la
    aplicacion

--- gen/com/moviles/yamba/R.java
+++ gen/com/moviles/yamba/R.java
@@ -22,12 +22,14 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int background=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080003;
         public static final int buttonTweet=0x7f080000;
         public static final int editStatus=0x7f080001;
+        public static final int textViewCount=0x7f080002;
     }
     public static final class layout {
         public static final int activity_status=0x7f030000;
@@ -41,6 +43,11 @@ public final class R {
         public static final int button_tweet=0x7f050003;
         public static final int hello_world=0x7f050001;
         public static final int hint_status=0x7f050004;
+        public static final int password=0x7f050009;
+        public static final int password_summary=0x7f050008;
+        public static final int textCount=0x7f050005;
+        public static final int username=0x7f050006;
+        public static final int username_summary=0x7f050007;
     }
     public static final class style {
         /** 
